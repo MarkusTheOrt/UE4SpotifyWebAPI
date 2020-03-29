e3d9984aba1062eb26437b80d21fbb6e9291ac81
Markus Ort <markus@gameplayuniver.se>
2020-03-29 20:29:09 +0200

    Deleted old Version (Still available in branches)

--- Source/SpotifyTest/SpotifyTest.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "SpotifyTest.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpotifyTest, "SpotifyTest" );
